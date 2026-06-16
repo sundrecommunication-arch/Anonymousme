a61a65df27d9e97df67c3d697b4c1cf360eea080
sundrecommunication-arch <sundrecommunication@gmail.com>
2026-06-16 07:38:45 +0100

    fix private key formatting

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     type: 'service_account',
     project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.FIREBASE_PRIVATE_KEY.includes('\\n') ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : process.env.FIREBASE_PRIVATE_KEY,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   })
 });
